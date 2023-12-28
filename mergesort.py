184ca59d597e40dd94ea13ae1346e24f933423ee
Pushpak Katkhede <36862277+katkhedepushpak@users.noreply.github.com>
2023-12-28 00:29:12 -0800

    mergesort

---
+++ mergesort.py
@@ -0,0 +1,33 @@
+import sys, time
+import random
+
+sys.setrecursionlimit(100000)
+
+def merging(left_a, right_a):
+    temp_array = []
+    x = y = 0
+    while x < len(left_a) and y < len(right_a):
+        if left_a[x] < right_a[y]:
+            temp_array.append(left_a[x])
+            x += 1
+        else:
+            temp_array.append(right_a[y])
+            y += 1
+    temp_array.extend(left_a[x:])
+    temp_array.extend(right_a[y:])
+    return temp_array
+
+
+def mergesort(Arr):
+    n = len(Arr)
+    if n == 1:
+        return Arr
+    middle = n // 2
+    left_array = mergesort(Arr[:middle])
+    right_array = mergesort(Arr[middle:])
+
+    return merging(left_array, right_array)
+
+
+A = []
+print(mergesort(A))
